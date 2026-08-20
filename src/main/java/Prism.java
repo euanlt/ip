25a0ca1c8535a930bb73bd2d742d1912be340487
euanlt <euantan5@gmail.com>
2026-08-20 20:52:19 +0800

    Rename chatbot

---
+++ src/main/java/Prism.java
@@ -0,0 +1,10 @@
+public class Prism {
+    public static void main(String[] args) {
+        String banner = " ____       _               \n"
+                + "|  _ \\ _ __(_)___ _ __ ___  \n"
+                + "| |_) | '__| / __| '_ ` _ \\ \n"
+                + "|  __/| |  | \\__ \\ | | | | |\n"
+                + "|_|   |_|  |_|___/_| |_| |_|\n";
+        System.out.println(banner);
+    }
+}
