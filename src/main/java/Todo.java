05248dca3fad34791eafb6d42c844fe7fc3f24b8
euanlt <euantan5@gmail.com>
2026-08-21 00:06:34 +0800

    Refactored code to use enums for Todos, Deadlines and Events

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,11 +1,8 @@
 public class Todo extends Task{
 
     public Todo(String description){
-        super(description);
+        super(description, TaskType.TODO);
     }
 
-    @Override
-    public String toString() {
-        return "[T]" + super.toString();
-    }
+
 }
